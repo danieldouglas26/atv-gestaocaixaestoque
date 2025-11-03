e8064dfe2a92b38f9ab3dd104db29f22c9852575
danieldouglas261 <danieldouglas26@outlook.com>
2025-11-03 20:53:54 -0300

    Fix: Alterando nome do projeto frontend e subindo backend

---
+++ syspdv_backend/src/main/java/com/fatesg/syspdv_backend/SyspdvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fatesg.syspdv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyspdvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyspdvBackendApplication.class, args);
+	}
+
+}
---
+++ syspdv_backend/src/test/java/com/fatesg/syspdv_backend/SyspdvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatesg.syspdv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyspdvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
