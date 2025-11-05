f027693d00eed65bcfaf134647a2213a8a0397b8
danieldouglas261 <danieldouglas26@outlook.com>
2025-11-04 21:06:04 -0300

    Fix: Corrijindo bug do primeng

--- syspdv_frontend/src/app/app.config.ts
+++ syspdv_frontend/src/app/app.config.ts
@@ -2,15 +2,28 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations(),
-    MessageService
+    MessageService,
+
+
+    provideAnimationsAsync(),
+
+
+    providePrimeNG({
+      theme: {
+        preset: Aura
+
+
+      }
+    })
   ]
 };
