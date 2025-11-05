32e8ca1320192abc801c522e7055fcc46cdf30c5
danieldouglas261 <danieldouglas26@outlook.com>
2025-11-05 15:55:21 -0300

    Refactor: Modificando user model para os novos dados vindo da API de fato

--- syspdv_frontend/src/app/core/models/user.model.ts
+++ syspdv_frontend/src/app/core/models/user.model.ts
@@ -1,13 +1,47 @@
+// Modelo do Usuário no Sistema
 export type UserRole = 'ADMIN' | 'OPERADOR';
 
 export interface User {
   id: number;
   nome: string;
   email: string;
-  perfil: UserRole;
+  perfil: UserRole; // 'ADMIN' ou 'OPERADOR'
 }
 
-export interface AuthResponse {
-  user: User;
-  token: string;
+// Modelo da Resposta de Login
+export interface ApiAuthResponse {
+  sucesso: boolean;
+  mensagem: string;
+  usuario: ApiUser;
+  tokenSessao: string;
+}
+
+// Modelo do Usuário
+export interface ApiUser {
+  id: number;
+  nomeCompleto: string;
+  email: string;
+  perfil: string; // "ADM" ou "OPR"
+  perfilDescricao: string;
+  status: string;
+  statusDescricao: string;
+}
+
+// Modelo para criação/atualização de usuário
+export interface UserPayload {
+  nomeCompleto: string;
+  email: string;
+  senha?: string; // Senha é opcional na atualização
+  perfil: string; // "ADM" ou "OPR"
+  status: string; // "A" ou "I"
+}
+
+// Modelo do Produto
+export interface Produto {
+  id: number;
+  codigo: string;
+  nome: string;
+  categoria: string;
+  quantidadeEstoque: number;
+  precoUnitario: number;
 }
